e3340c54cd18bac906578af0876981fae01e6a13
nataliemolendyk <nmolendyk1@babson.edu>
2026-02-03 11:01:48 -0500

    Session 5

---
+++ code/s05_review.py
@@ -0,0 +1,15 @@
+a = input("Enter an integer: ")
+a = int(a)
+print(type(a))
+
+# check odd or even
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+a = 2
+1 + a  # an expression, which evaluates to 3, but does not do anything with it
+
+print(1 + a) # a statement, which prints the result
+
