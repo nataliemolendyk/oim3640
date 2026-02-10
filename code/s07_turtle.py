90c51b541d885a8509ddad1db6cb8f7ef3033ef5
nataliemolendyk <nmolendyk1@babson.edu>
2026-02-10 11:15:03 -0500

    Push Information to READ.ME

---
+++ code/s07_turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+
+t.forward(100)
+t.left(90)
+
+
+
+# turtle.done() # makes sure the tutle graphics window stays open until you close it
+turtle.mainloop() # this is the same as turtle.done() but it is more corss-platform compatible
