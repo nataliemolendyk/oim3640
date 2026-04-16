d7961842ebea7a9144c49a50ce0c2e0129af397c
nataliemolendyk <nmolendyk1@babson.edu>
2026-04-16 11:04:51 -0400

    mbta flask app

---
+++ project 3/mbta.py
@@ -0,0 +1,16 @@
+from distro import name
+from flask import Flask, render_template
+from PROTOTYPE import find_stop_near
+
+app = Flask(__name__)
+
+@app.route('/station/<place_name>')
+def station(place_name):
+    stop_name, accessible = find_stop_near(place_name)
+
+    return render_template(
+        "station.html",
+        place=place_name,
+        stop=stop_name,
+        accessible=accessible
+    )
